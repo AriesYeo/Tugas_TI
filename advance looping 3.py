b5e838683c94623e64ce9edd66368310699f4649
AriesYeo <NeziTzuu@gmail.com>
2024-10-02 21:53:45 +0700

    Blood ANd tears 4
    haha...

---
+++ advance looping 3.py
@@ -0,0 +1,51 @@
+# numbers = []
+# i = 1
+# while i < 16:
+#     i += 1
+#     if i == 5:
+#         continue
+#     if i == 7:
+#         continue
+#     if i == 9:
+#         continue
+#     if i == 10:
+#         continue
+#     if i == 11:
+#         continue
+#     if i == 13:
+#         continue
+#     if i == 14:
+#         continue
+#     if i == 15:
+#         continue
+#     if i == 16:
+#         continue
+#     numbers.append(i)
+# for number in numbers:
+#     print (number, end="," )
+#     if number == 12:
+#         print (16, end="")
+
+
+
+# Soal B 
+# num = 18
+
+# while num >=9:
+#     num -= 3
+#     print(num, end=",")
+#     if num == 6:
+#         print( 3,end = "")
+
+    
+# Soal C
+numbers = []
+i = 0
+while i <= 35:
+    i += 7
+    numbers.append(i)
+for number in numbers:
+    print (number, end=" ," )
+    if number == 42:
+        print (49, end="")
+    
